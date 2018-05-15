416b4cc5c8e21f12c1739be35803e7f70fe0f3a5
rofinn <rory.finnegan@gmail.com>
2018-05-15 12:54:03 -0500

    Add Sockets module.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1468,6 +1468,20 @@ import Compat.Markdown
     @test stderr === STDERR
 end
 
+# 0.7.0-DEV.3666
+module TestSockets
+    using Compat
+    using Compat.Sockets
+    using Compat.Test
+
+    @test isdefined(@__MODULE__, :UDPSocket)
+    @test isdefined(@__MODULE__, :connect)
+    @test isdefined(@__MODULE__, :listen)
+    @test isdefined(@__MODULE__, :recv)
+
+    @test ip"127.0.0.1".host == UInt32(2130706433)
+end
+
 # 0.7.0-DEV.3526
 module TestNames
     export foo
