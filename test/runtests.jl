af45b90a8ce0aed24f7d910d1efd8d9c4641bf3f
Takafumi Arakaki <aka.tkf@gmail.com>
2020-04-28 22:51:22 -0700

    Add mergewith[!](combine, dicts...) (#694)
    * Add mergewith[!](combine, dicts...)
    
    * Update Project.toml
    
    Co-Authored-By: Martin Holters <martin.holters@hsu-hh.de>
    
    * Fix link in README.md
    
    * Define a helper function only if needed
    
    Co-authored-by: Martin Holters <martin.holters@hsu-hh.de>

--- test/runtests.jl
+++ test/runtests.jl
@@ -308,4 +308,33 @@ end
     @test (@NamedTuple {a::Int, b}) === NamedTuple{(:a, :b),Tuple{Int,Any}}
 end
 
+struct NonFunctionCallable end
+(::NonFunctionCallable)(args...) = +(args...)
+
+@testset "mergewith" begin
+    d1 = Dict("A" => 1, "B" => 2)
+    d2 = Dict("B" => 3.0, "C" => 4.0)
+    @test mergewith(+, d1, d2) == Dict("A" => 1, "B" => 5, "C" => 4)
+    @test mergewith(*, d1, d2) == Dict("A" => 1, "B" => 6, "C" => 4)
+    @test mergewith(-, d1, d2) == Dict("A" => 1, "B" => -1, "C" => 4)
+    @test mergewith(NonFunctionCallable(), d1, d2) == Dict("A" => 1, "B" => 5, "C" => 4)
+    @test foldl(mergewith(+), [d1, d2]; init=Dict{Union{},Union{}}()) ==
+        Dict("A" => 1, "B" => 5, "C" => 4)
+end
+
+@testset "mergewith!" begin
+    d1 = Dict("A" => 1, "B" => 3, "C" => 4)
+    d2 = Dict("B" => 3, "C" => 4)
+    mergewith!(+, d1, d2)
+    @test d1 == Dict("A" => 1, "B" => 6, "C" => 8)
+    mergewith!(*, d1, d2)
+    @test d1 == Dict("A" => 1, "B" => 18, "C" => 32)
+    mergewith!(-, d1, d2)
+    @test d1 == Dict("A" => 1, "B" => 15, "C" => 28)
+    mergewith!(NonFunctionCallable(), d1, d2)
+    @test d1 == Dict("A" => 1, "B" => 18, "C" => 32)
+    @test foldl(mergewith!(+), [d1, d2]; init=empty(d1)) ==
+        Dict("A" => 1, "B" => 21, "C" => 36)
+end
+
 nothing
