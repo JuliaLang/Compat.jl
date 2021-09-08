6e04e3a1249d2163d98f439984b1ed37e30a4ff8
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2021-09-08 03:26:09 -0400

    add Returns (#751)

--- src/Compat.jl
+++ src/Compat.jl
@@ -1102,6 +1102,25 @@ if VERSION < v"1.7.0-DEV.1106"
     export current_exceptions
 end
 
+# https://github.com/JuliaLang/julia/pull/39794
+if VERSION < v"1.7.0-DEV.793"
+    export Returns
+
+    struct Returns{V} <: Function
+        value::V
+        Returns{V}(value) where {V} = new{V}(value)
+        Returns(value) = new{Core.Typeof(value)}(value)
+    end
+
+    (obj::Returns)(args...; kw...) = obj.value
+    function Base.show(io::IO, obj::Returns)
+        show(io, typeof(obj))
+        print(io, "(")
+        show(io, obj.value)
+        print(io, ")")
+    end
+end
+
 include("iterators.jl")
 include("deprecated.jl")
 
