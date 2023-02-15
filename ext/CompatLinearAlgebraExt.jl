9434e156e5a6587359d53de2f4ed2a50e1c076dd
Oscar Smith <oscardssmith@gmail.com>
2023-02-15 14:37:05 -0500

    Update CompatLinearAlgebraExt.jl

--- ext/CompatLinearAlgebraExt.jl
+++ ext/CompatLinearAlgebraExt.jl
@@ -1,6 +1,6 @@
 module CompatLinearAlgebraExt
 using LinearAlgebra
 
-Base.@deprecate_binding set_num_threads LinearAlgebra.BLAS.set_num_threads false
-Base.@deprecate_binding get_num_threads LinearAlgebra.BLAS.get_num_threads false
+Base.@deprecate_binding Compat.set_num_threads LinearAlgebra.BLAS.set_num_threads false
+Base.@deprecate_binding Compat.get_num_threads LinearAlgebra.BLAS.get_num_threads false
 end
