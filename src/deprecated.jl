2ef70bebd2beb4cc448652dc12d5846c0508bd39
Martin Holters <martin.holters@hsu-hh.de>
2024-10-29 13:13:47 +0100

    Remove deprecations

--- src/deprecated.jl
+++
@@ -1,9 +0,0 @@
-# deprecation is done in the CompatLinearAlgebraExt package extension, but
-# the functions need to be declared here
-function set_num_threads end
-function get_num_threads end
-
-Base.@deprecate_binding parseatom Meta.parseatom false
-Base.@deprecate_binding parseall Meta.parseall false
-import UUIDs
-Base.@deprecate_binding uuid5 UUIDs.uuid5 true
