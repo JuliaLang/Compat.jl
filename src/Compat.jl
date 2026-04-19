24c2e8d904b1b7a53427b9b2b4e3b1f84f615d32
Dilum Aluthge <5619885+DilumAluthge@users.noreply.github.com>
2026-04-19 03:51:27 -0400

    Add `SHA256` struct (and related methods) from PR JuliaLang/julia#61555 (#851)

--- src/Compat.jl
+++ src/Compat.jl
@@ -1160,7 +1160,7 @@ if VERSION < v"1.12.0-DEV.974"  # contrib/commit-name.sh 2635dea
 else
     using Base: insertdims, _insertdims
 end
-                                
+
 # https://github.com/JuliaLang/julia/pull/54653: add Fix
 @static if !isdefined(Base, :Fix) # VERSION < v"1.12.0-DEV.981"
     @static if !isdefined(Base, :_stable_typeof)
@@ -1281,6 +1281,38 @@ end
     """ :(@__FUNCTION__)
 
     export @__FUNCTION__
+end
+
+# https://github.com/JuliaLang/julia/pull/61555: Add a `Base.SHA256` struct into Base
+@static if !isdefined(Base, :SHA256)
+    struct SHA256
+        bytes::NTuple{32, UInt8}
+    end
+    function SHA256(bytes::Vector{UInt8})
+        length(bytes) == 32 ||
+            throw(ArgumentError("wrong number of bytes for SHA1: Expected 32 bytes, got $(length(bytes))"))
+        return SHA256(ntuple(i->bytes[i], Val(32)))
+    end
+    SHA256(s::AbstractString) = SHA256(hex2bytes(s))
+    Base.parse(::Type{SHA256}, s::AbstractString) = SHA256(s)
+    function Base.tryparse(::Type{SHA256}, s::AbstractString)
+        try
+            return parse(SHA256, s)
+        catch e
+            if isa(e, ArgumentError)
+                return nothing
+            end
+            rethrow(e)
+        end
+    end
+
+    Base.string(hash::SHA256) = bytes2hex(hash.bytes)
+    Base.print(io::IO, hash::SHA256) = bytes2hex(io, hash.bytes)
+    Base.show(io::IO, hash::SHA256) = print(io, SHA256str * "(\"", hash, "\")")
+
+    Base.isless(a::SHA256, b::SHA256) = isless(a.bytes, b.bytes)
+    Base.hash(a::SHA256, h::UInt) = hash((SHA256, a.bytes), h)
+    Base.:(==)(a::SHA256, b::SHA256) = a.bytes == b.bytes
 end
 
 include("deprecated.jl")
